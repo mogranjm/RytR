90b993d74a2f7663705831a66657a548e8cc6ac0
James Morgan <ja.morgan1@outlook.com>
2019-04-01 11:45:22 +1100

    fix manuscript.docx styles-template location

--- R/manuscript.R
+++ R/manuscript.R
@@ -1,7 +1,7 @@
 manuscript <- function(toc=TRUE) {
 
     # get locations of resource files
-    docx_template <- 'inst/rmarkdown/templates/manuscript/resources/style-reference.docx'
+    docx_template <- 'inst/rmarkdown/templates/manuscript/skeleton/resources/style-reference.docx'
 
     rmarkdown::word_document(
         reference_docx = docx_template,
