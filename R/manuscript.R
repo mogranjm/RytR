01a9838b07af61a9a2593ddd1470ed6b934bf46a
James Morgan <ja.morgan1@outlook.com>
2019-04-01 13:05:22 +1100

    add functions to locate template latex and docx files in package directory

--- R/manuscript.R
+++ R/manuscript.R
@@ -1,10 +1,3 @@
-manuscript <- function(toc=TRUE) {
-
-    # get locations of resource files
-    docx_template <- 'resources/style-reference.docx'
-
-    rmarkdown::word_document(
-        reference_docx = docx_template,
-        toc = toc
-    )
+manuscript <- function(...) {
+    word_document_format("manuscript", ...)
 }
---
+++ R/utils.R
@@ -0,0 +1,28 @@
+# Helper function to locate a template file in the RytR package
+# Searches in RytR/inst/rmarkdown/templates/*Template Name*/resources/ for the specified template file
+find_resource <- function(template, file = 'template.tex'){
+    res <- system.file(
+        "rmarkdown", "templates", template, "resources", file, package = "RytR"
+    )
+    if(res == "") stop(
+        "Couldn't locate ", template, "/resources/", file, call. = FALSE
+    )
+    res
+}
+
+# Helper function to create a custom word format, based on word_document
+# Including custom latex and docx style template
+word_document_format <- function(
+    format,
+    pandoc_template = find_resource(format, 'style-reference.docx'),
+    docx_template = find_resource(format, 'template.tex'),
+    ...
+){
+    fmt <- rmarkdown::word_document(
+        ...,
+        template = pandoc_template,
+        reference_docx = docx_template
+    )
+    fmt$inherits <- "word_document"
+    fmt
+}
