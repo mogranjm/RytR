22d54e99e54304e2f9cf99c76e274989451a0bb1
James Morgan <ja.morgan1@outlook.com>
2019-06-17 05:15:47 +0000

    various documentation

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,8 @@
-# Helper function to locate a template file in the papeRs package
-# Searches in papeRs/inst/rmarkdown/templates/*Template Name*/resources/ for the specified template file
+#' A Helper function used for locating template files.
+#'
+#' Locates the custom .tex template file in the rmarkdown/templates directory
+#' @param template Name of target document template
+#' @param file Name of the target tex template for this document
 find_resource <- function(template, file = 'template.tex'){
     res <- system.file(
         "rmarkdown", "templates", template, "resources", file, package = "papeRs"
@@ -11,8 +14,13 @@ find_resource <- function(template, file = 'template.tex'){
 }
 
 
-# Helper function to create a custom word format, based on word_document
 # Including custom latex and docx style template
+#' A Helper function to create a custom word format, based on word_document.
+#'
+#' Locates the custom .tex template file in the rmarkdown/templates directory
+#' @param template_name Name of target document template
+#' @param docx_template Name of the target docx template for this document
+#' @param pandoc_args Arguments passed to Pandoc
 word_document_format <- function(
     template_name,
     docx_template = find_resource(template_name, 'style-reference.docx'),
