cb3abdc049579df44a2c1b8802228c5369f85e85
James Morgan <ja.morgan1@outlook.com>
2019-06-24 12:52:53 +1000

    update documentation

--- R/manuscript.R
+++ R/manuscript.R
@@ -1,6 +1,6 @@
 #' Knit R Markdown to MS Word Manuscript
 #'
-#' Render Meeting Manuscript as a .docx using the custom .tex file and .lua filters
+#' Render Research Manuscript as a .docx using the custom .tex file and .lua filters
 #' @param ... Arguments passed to \code{rmarkdown::\link{word_document_format}()}.
 #' @export
 #' @example
